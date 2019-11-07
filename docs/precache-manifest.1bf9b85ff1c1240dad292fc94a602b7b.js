7a31cc7c0ca8657f9908d1afea046456653a0b7f
Huw Wilkins <huwshimi@gmail.com>
2019-11-07 16:53:05 +1100

    Build docs for 0.2.1.

---
+++ docs/precache-manifest.1bf9b85ff1c1240dad292fc94a602b7b.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15414dd8ee6163224d95577a5f407c1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.47b6989d9266e4aa6407.bundle.js"
+  },
+  {
+    "revision": "4c0e65312c22f58a72ee",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.47b6989d9266e4aa6407.bundle.js"
+  },
+  {
+    "url": "vendors~main.47b6989d9266e4aa6407.bundle.js"
+  }
+]);
