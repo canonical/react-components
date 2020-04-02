2f129274d04cefc25e274e169bed1e13ac3936c1
Huw Wilkins <huw.wilkins@canonical.com>
2020-04-02 13:49:59 +1100

    Build docs for 0.6.0.

---
+++ docs/precache-manifest.a44b1f8d917d5bae2f4896139a0a7c26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85fff1e40375bd6c75d7a54ca93abc8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.81f42632f23f08721376.bundle.js"
+  },
+  {
+    "url": "runtime~main.81f42632f23f08721376.bundle.js"
+  },
+  {
+    "revision": "869f884f264596087a36",
+    "url": "static/css/main.6139caf5.chunk.css"
+  },
+  {
+    "url": "vendors~main.81f42632f23f08721376.bundle.js"
+  },
+  {
+    "url": "vendors~main.81f42632f23f08721376.bundle.js.LICENSE.txt"
+  }
+]);
