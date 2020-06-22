741ef724215f819a1d3afa561aca71f75f18c0ac
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-06-22 12:00:34 +0100

    Add basic structure of Search and Filter component

---
+++ src/components/SearchAndFilter/SearchAndFilter.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import SearchBox from "../SearchBox";
+
+const SearchAndFilter = ({ externallyControlled = false, onChange }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchOnChange = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    if (externallyControlled) {
+      onChange(searchTerm);
+    }
+  };
+
+  return (
+    <div className="search-and-filter">
+      <SearchBox
+        externallyControlled={externallyControlled}
+        placeholder="Search and filter"
+        onChange={(searchTerm) => searchOnChange(searchTerm)}
+        value={searchTerm}
+      />
+    </div>
+  );
+};
+
+SearchAndFilter.propTypes = {
+  externallyControlled: PropTypes.bool,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+};
+
+export default SearchAndFilter;
