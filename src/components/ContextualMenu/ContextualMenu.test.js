2ff720694d1a0b5feb9dc1871ac87bca1b3d2180
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-06-23 12:22:39 +0100

    Use attribute assertion instead of snapshots

--- src/components/ContextualMenu/ContextualMenu.test.js
+++ src/components/ContextualMenu/ContextualMenu.test.js
@@ -6,13 +6,17 @@ import ContextualMenu from "./ContextualMenu";
 describe("Contextual Menu ", () => {
   it("renders instance but not visible", () => {
     const wrapper = shallow(<ContextualMenu>Test content</ContextualMenu>);
-    expect(wrapper).toMatchSnapshot();
+    expect(
+      wrapper.find(".p-contextual-menu__dropdown").prop("aria-hidden")
+    ).toEqual(true);
   });
 
   it("renders visible instance", () => {
     const wrapper = shallow(
       <ContextualMenu visible={true}>Test content</ContextualMenu>
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(
+      wrapper.find(".p-contextual-menu__dropdown").prop("aria-hidden")
+    ).toEqual(false);
   });
 });
