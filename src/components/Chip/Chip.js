2434a6b5afdce2b5ef09adc16676213eb65d280b
Anthony Dillon <me@anthonydillon.com>
2020-06-23 12:18:11 +0100

    Add lead-value chip

--- src/components/Chip/Chip.js
+++ src/components/Chip/Chip.js
@@ -3,16 +3,24 @@ import PropTypes from "prop-types";
 
 import "./Chip.scss";
 
-const Chip = ({ value }) => {
+const generateLead = (lead) => {
+  if (!lead && typeof lead !== "string") {
+    return "";
+  }
+  return `${lead.toUpperCase()}: `;
+}
+
+const Chip = ({ value, lead }) => {
   return (
     <div className="p-chip">
-      { value }
+      { generateLead(lead) }{ value }
     </div>
   );
 };
 
 Chip.propTypes = {
   value: PropTypes.string,
+  lead: PropTypes.string,
 };
 
 export default Chip;
