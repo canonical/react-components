5313d28115e7e2010b9265fb211243ec6f9108eb
Huw Wilkins <huw.wilkins@canonical.com>
2020-07-31 13:11:09 +1000

    Allow nodes to be passed for field content. Pass wrapper and label classes to fields.

--- src/components/Textarea/Textarea.js
+++ src/components/Textarea/Textarea.js
@@ -12,20 +12,24 @@ const Textarea = ({
   help,
   id,
   label,
+  labelClassName,
   onKeyUp,
   required,
   stacked,
   style,
   success,
+  wrapperClassName,
   ...props
 }) => {
   return (
     <Field
       caution={caution}
+      className={wrapperClassName}
       error={error}
       forId={id}
       help={help}
       label={label}
+      labelClassName={labelClassName}
       required={required}
       stacked={stacked}
       success={success}
@@ -56,18 +60,20 @@ const Textarea = ({
 };
 
 Textarea.propTypes = {
-  caution: PropTypes.string,
+  caution: PropTypes.node,
   className: PropTypes.string,
-  error: PropTypes.string,
+  error: PropTypes.node,
   grow: PropTypes.bool,
   help: PropTypes.node,
   id: PropTypes.string,
   label: PropTypes.node,
+  labelClassName: PropTypes.string,
   onKeyUp: PropTypes.func,
   required: PropTypes.bool,
   stacked: PropTypes.bool,
   style: PropTypes.object,
-  success: PropTypes.string,
+  success: PropTypes.node,
+  wrapperClassName: PropTypes.string,
 };
 
 export default Textarea;
