4aad5b9390f09efa0dc0ae6dfad418db194cfdd7
Kit Randel <kit.randel@canonical.com>
2020-08-07 15:34:23 +1200

    Import type.

--- src/components/PaginationItem/PaginationItem.tsx
+++ src/components/PaginationItem/PaginationItem.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
-import React, { MouseEventHandler } from "react";
+import React from "react";
+import type { MouseEventHandler } from "react";
 
 type Props = {
   number: number;
