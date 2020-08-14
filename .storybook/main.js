914170e76e12a4762ca32d3ef815532514e96159
Huw Wilkins <huw.wilkins@canonical.com>
2020-08-14 16:06:59 +1000

    Improve theme to look more like Vanilla. Upgrade Storybook. Replace Knobs with Controls.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ["../src/**/*.stories.@(js|ts|jsx|tsx|mdx)"],
   addons: [
-    "@storybook/addon-knobs/register",
     "@storybook/addon-docs/preset",
+    "@storybook/addon-controls",
     "@storybook/preset-create-react-app",
   ],
 };
---
+++ .storybook/vanillaish.js
@@ -0,0 +1,39 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+
+  colorPrimary: "#fff",
+  colorSecondary: "rgba(0,0,0,0.05)",
+
+  // UI
+  appBg: "#fff",
+  appContentBg: "#fff",
+  appBorderColor: "#cdcdcd",
+  appBorderRadius: ".125rem",
+
+  // Typography
+  fontBase:
+    '"Ubuntu", -apple-system, "Segoe UI", "Roboto", "Oxygen", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+  fontCode: '"Ubuntu Mono", Consolas, Monaco, Courier, monospace',
+
+  // Text colors
+  textColor: "#111",
+  textInverseColor: "#111",
+
+  // Toolbar default and active colors
+  barTextColor: "#111",
+  barSelectedColor: "#666",
+  barBg: "#fff",
+
+  // Form colors
+  inputBg: "#fff",
+  inputBorder: "#cdcdcd",
+  inputTextColor: "#111",
+  inputBorderRadius: ".125rem",
+
+  brandTitle: "Vanilla React Library",
+  brandUrl: "/",
+  brandImage:
+    "https://assets.ubuntu.com/v1/746e552e-vanilla_black-orange_hex.svg",
+});
