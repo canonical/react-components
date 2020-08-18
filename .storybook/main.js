d20eb61d0273a90615eee160036c8898dd2835f4
Huw Wilkins <huw.wilkins@canonical.com>
2020-08-18 15:21:17 +1000

    Update config after merging with #243 and upgrade Storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,7 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.@(js|ts|jsx|tsx|mdx)"],
-  addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-docs/preset",
-    "@storybook/addon-controls",
-  ],
+  addons: ["@storybook/addon-docs/preset", "@storybook/addon-controls"],
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.scss$/,
