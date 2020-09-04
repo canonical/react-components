98bd713161c91ba7a271ee95ddef31dcb2cb6cca
Bartek Szopka <bartek.szopka@canonical.com>
2020-09-04 15:37:40 +0200

    Initial proof of concept build of react-table use

---
+++ src/components/ModularTable/ModularTable.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+// TODO:
+// import PropTypes from "prop-types";
+import { useTable } from "react-table";
+
+import Table from "../Table";
+import TableRow from "../TableRow";
+import TableHeader from "../TableHeader";
+import TableCell from "../TableCell";
+
+function ModularTable({ data, columns }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
+
+  return (
+    <Table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <TableRow {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <TableHeader
+                {...column.getHeaderProps([
+                  {
+                    className: column.className,
+                  },
+                ])}
+              >
+                {column.render("Header")}
+              </TableHeader>
+            ))}
+          </TableRow>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <TableRow {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <TableCell
+                    {...cell.getCellProps([
+                      {
+                        className: cell.column.className,
+                      },
+                    ])}
+                  >
+                    {cell.render("Cell")}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+}
+
+export default ModularTable;
