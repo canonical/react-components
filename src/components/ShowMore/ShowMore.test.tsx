c69b614b60beb591edae6b067d3839be1fb5091c
Bartek Szopka <bartek.szopka@canonical.com>
2020-10-05 15:34:31 +0200

    Update ShowMore component to design spec

--- src/components/ShowMore/ShowMore.test.tsx
+++ src/components/ShowMore/ShowMore.test.tsx
@@ -7,7 +7,11 @@ describe("<ShowMore />", () => {
   // snapshot tests
   it("renders and matches the snapshot", () => {
     const component = shallow(
-      <ShowMore label="Show more" onClick={() => false} />
+      <ShowMore
+        summary="Showing some items"
+        label="Show more"
+        onClick={() => false}
+      />
     );
 
     expect(component).toMatchSnapshot();
@@ -24,11 +28,17 @@ describe("<ShowMore />", () => {
     ).toBeTruthy();
   });
 
-  it("can handle button click events", () => {
+  it("can handle click events", () => {
     const onClick = jest.fn();
     const component = shallow(<ShowMore label="Show more" onClick={onClick} />);
 
-    component.find("button").first().simulate("click");
+    component
+      .find("a")
+      .first()
+      .simulate("click", {
+        // when shallow rendering enzyme doesn't provide event object to handlers, so we need to mock it
+        preventDefault: () => false,
+      });
     expect(onClick).toHaveBeenCalled();
   });
 });
