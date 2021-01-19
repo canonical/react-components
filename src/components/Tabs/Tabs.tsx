5762aa982a3e04a2a7bd7a25f12e0df506d18038
Huw Wilkins <huw.wilkins@canonical.com>
2021-01-19 14:42:02 +1100

    Support custom components as tab links. Migrate Tabs to TypeScript.

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,12 +1,31 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import React from "react";
+import type { HTMLProps, ElementType, ReactNode, ComponentType } from "react";
 
-const Tabs = ({ className, links, listClassName }) => {
+export type TabLink<P = null> = {
+  active?: boolean;
+  className?: string;
+  component?: ElementType | ComponentType<P>;
+  label: ReactNode;
+  listItemClassName?: string;
+} & (HTMLProps<HTMLElement> | P);
+
+type Props<P = null> = {
+  links: TabLink<P>[];
+  className?: string;
+  listClassName?: string;
+};
+
+const Tabs = <P,>({
+  className,
+  links,
+  listClassName,
+}: Props<P>): JSX.Element => {
   return (
     <nav className={classNames("p-tabs", className)}>
       <ul className={classNames("p-tabs__list", listClassName)}>
-        {links.map((link) => {
+        {links.map((link, i) => {
           const {
             active,
             className,
@@ -19,7 +38,7 @@ const Tabs = ({ className, links, listClassName }) => {
           return (
             <li
               className={classNames("p-tabs__item", listItemClassName)}
-              key={label}
+              key={i}
             >
               <Component
                 aria-selected={active}
@@ -58,11 +77,7 @@ Tabs.propTypes = {
       /**
        * Optional component to be used instead of the default "a" element.
        */
-      component: PropTypes.oneOfType([
-        PropTypes.element,
-        PropTypes.node,
-        PropTypes.object,
-      ]),
+      component: PropTypes.elementType,
       /**
        * Label to be displayed inside the tab link.
        */
