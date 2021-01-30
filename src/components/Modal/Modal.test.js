ba3af90080b02fba7420c10f4a3adbb041f7dec6
Jeff Pihach <jeff.pihach@canonical.com>
2021-01-29 20:31:50 -0600

    Expand testing coverage on the Modal component.

--- src/components/Modal/Modal.test.js
+++ src/components/Modal/Modal.test.js
@@ -12,4 +12,37 @@ describe("Modal ", () => {
     );
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("displays buttons that have been supplied", () => {
+    const wrapper = shallow(
+      <Modal
+        buttons={[
+          <button key={1}>Button 1</button>,
+          <button key={2}>Button 2</button>,
+        ]}
+      >
+        Button tester
+      </Modal>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("if no buttons are provided it does not render the button wrapper", () => {
+    const wrapper = shallow(
+      <Modal close={jest.fn()} title="Confirm delete">
+        Are you sure?
+      </Modal>
+    );
+    expect(wrapper.find(".p-modal__button-row")).toEqual({});
+  });
+
+  it("does not display a header if a title is not provided", () => {
+    const wrapper = shallow(<Modal>Bare bones.</Modal>);
+    expect(wrapper.find(".p-modal__header")).toEqual({});
+  });
+
+  it("does not display a close button if no close handler is provided", () => {
+    const wrapper = shallow(<Modal title="some title">Bare bones.</Modal>);
+    expect(wrapper.find(".p-modal__close")).toEqual({});
+  });
 });
