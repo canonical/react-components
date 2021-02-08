ee01295d31eaace04d39c569714842de12e18a1e
Bartek Szopka <bartek.szopka@canonical.com>
2021-02-08 14:41:09 +0100

    Improve tests for CodeSnippet

--- src/components/CodeSnippet/CodeSnippet.test.tsx
+++ src/components/CodeSnippet/CodeSnippet.test.tsx
@@ -1,23 +1,24 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import React from "react";
 
 import CodeSnippet from "./CodeSnippet";
 
 describe("CodeSnippet ", () => {
-  it("can render", () => {
-    const wrapper = shallow(<CodeSnippet blocks={[{ code: "Test" }]} />);
-    expect(wrapper).toMatchSnapshot();
+  it("renders a code block", () => {
+    const wrapper = mount(<CodeSnippet blocks={[{ code: "Test" }]} />);
+    expect(wrapper.find(".p-code-snippet__block").text()).toEqual("Test");
   });
 
-  it("can render snippet with a title", () => {
-    const wrapper = shallow(
+  it("renders title for a code block", () => {
+    const wrapper = mount(
       <CodeSnippet blocks={[{ title: "Title", code: "Test" }]} />
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find(".p-code-snippet__title").length).toBe(1);
+    expect(wrapper.find(".p-code-snippet__title").text()).toEqual("Title");
   });
 
-  it("can render snippet with multiple blocks", () => {
-    const wrapper = shallow(
+  it("renders multiple code blocks", () => {
+    const wrapper = mount(
       <CodeSnippet
         blocks={[
           { title: "Title", code: "Test" },
@@ -25,6 +26,80 @@ describe("CodeSnippet ", () => {
         ]}
       />
     );
-    expect(wrapper.find("CodeSnippetBlock").length).toBe(2);
+    expect(wrapper.find("pre").length).toBe(2);
+  });
+
+  it("renders line numbers", () => {
+    const multilineCode = `Test line 1;
+    Test line 2;
+    Test line 3;`;
+
+    const wrapper = mount(
+      <CodeSnippet blocks={[{ appearance: "numbered", code: multilineCode }]} />
+    );
+    const lines = wrapper.find(
+      ".p-code-snippet__block--numbered .p-code-snippet__line"
+    );
+    expect(lines.length).toBe(3);
+    expect(lines.first().text()).toEqual("Test line 1;");
+  });
+
+  it("renders default linux prompt icon", () => {
+    const wrapper = mount(
+      <CodeSnippet blocks={[{ appearance: "linuxPrompt", code: "Test" }]} />
+    );
+    expect(wrapper.find(".p-code-snippet__block--icon").length).toBe(1);
+  });
+
+  it("renders windows prompt icon", () => {
+    const wrapper = mount(
+      <CodeSnippet blocks={[{ appearance: "windowsPrompt", code: "Test" }]} />
+    );
+    expect(
+      wrapper.find(".p-code-snippet__block--icon.is-windows-prompt").length
+    ).toBe(1);
+  });
+
+  it("renders url icon", () => {
+    const wrapper = mount(
+      <CodeSnippet blocks={[{ appearance: "url", code: "Test" }]} />
+    );
+    expect(wrapper.find(".p-code-snippet__block--icon.is-url").length).toBe(1);
+  });
+
+  it("renders code block with line wrapping", () => {
+    const wrapper = mount(
+      <CodeSnippet blocks={[{ wrapLines: true, code: "Test" }]} />
+    );
+    expect(wrapper.find(".p-code-snippet__block.is-wrapped").length).toBe(1);
+  });
+
+  it("renders code block with a dropdown", () => {
+    const onChange = jest.fn();
+    const wrapper = mount(
+      <CodeSnippet
+        blocks={[
+          {
+            code: "Test",
+            dropdowns: [
+              {
+                options: [
+                  { value: "js", label: "JS" },
+                  { value: "css", label: "CSS" },
+                  { value: "html", label: "HTML" },
+                ],
+                value: "html",
+                onChange: onChange,
+              },
+            ],
+          },
+        ]}
+      />
+    );
+    const dropdowns = wrapper.find(".p-code-snippet__dropdown");
+    expect(dropdowns.length).toBe(1);
+    expect(dropdowns.first().prop("value")).toEqual("html");
+    dropdowns.first().simulate("change");
+    expect(onChange).toHaveBeenCalled();
   });
 });
