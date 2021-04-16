f5b15aa530b0f8a6da8704632f58becc162dc7e9
Scott Mason Nash <sowasred2012@gmail.com>
2021-04-16 12:30:20 +0100

    remove unnecessary CheckableInput snapshots

--- src/components/Checkbox/Checkbox.test.tsx
+++ src/components/Checkbox/Checkbox.test.tsx
@@ -1,11 +1,11 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import React from "react";
 
 import Checkbox from "./Checkbox";
 
 describe("Checkbox ", () => {
   it("renders a checkbox", () => {
-    const wrapper = shallow(<Checkbox label="Test checkbox"></Checkbox>);
-    expect(wrapper).toMatchSnapshot();
+    const wrapper = mount(<Checkbox label="Test checkbox"></Checkbox>);
+    expect(wrapper.find("input.p-checkbox__input").length).toBe(1);
   });
 });
