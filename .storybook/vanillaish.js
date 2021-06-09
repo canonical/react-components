106b8e54d55830db0b7b4e448ad8e4529bd09a2a
minkyngkm <minkyngkm@gmail.com>
2021-06-09 15:19:46 +0100

    update storybook's brandUrl

--- .storybook/vanillaish.js
+++ .storybook/vanillaish.js
@@ -33,7 +33,7 @@ export default create({
   inputBorderRadius: ".125rem",
 
   brandTitle: "Vanilla React Library",
-  brandUrl: "/",
+  brandUrl: "/react-components",
   brandImage:
     "https://assets.ubuntu.com/v1/746e552e-vanilla_black-orange_hex.svg",
 });
