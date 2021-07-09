cec2da5059c0474a99b8fdde3d15e99779d57aa7
Huw Wilkins <huw.wilkins@canonical.com>
2021-07-09 15:55:55 +1000

    Migrate the Chip component to typescript.

--- src/components/Chip/Chip.test.tsx
+++ src/components/Chip/Chip.test.tsx
@@ -5,29 +5,38 @@ import Chip from "./Chip";
 
 describe("Chip ", () => {
   it("renders default chip", () => {
-    const wrapper = shallow(<Chip value="positive" />);
+    const wrapper = shallow(<Chip onClick={jest.fn()} value="positive" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it("renders lead-value chip", () => {
-    const wrapper = shallow(<Chip lead="Owner" value="Bob" />);
+    const wrapper = shallow(
+      <Chip onClick={jest.fn()} lead="Owner" value="Bob" />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it("displays the value", () => {
-    const wrapper = mount(<Chip value="positive" />);
+    const wrapper = mount(<Chip onClick={jest.fn()} value="positive" />);
     expect(wrapper.find(".p-chip").text()).toBe("positive");
   });
 
   it("displays the lead-value", () => {
-    const wrapper = mount(<Chip lead="Owner" value="Bob" />);
+    const wrapper = mount(
+      <Chip onClick={jest.fn()} lead="Owner" value="Bob" />
+    );
     expect(wrapper.find(".p-chip").text()).toBe("OWNERBob"); // Colon and space added with CSS
   });
 
   it("displays the dismiss action", () => {
     const onDismiss = jest.fn();
     const wrapper = mount(
-      <Chip lead="Owner" value="Bob" onDismiss={onDismiss} />
+      <Chip
+        onClick={jest.fn()}
+        lead="Owner"
+        value="Bob"
+        onDismiss={onDismiss}
+      />
     );
     expect(wrapper.find(".p-chip__dismiss").exists()).toBe(true);
   });
@@ -35,7 +44,12 @@ describe("Chip ", () => {
   it("calls onDismiss when clicked", () => {
     const onDismiss = jest.fn();
     const wrapper = mount(
-      <Chip lead="Owner" value="Bob" onDismiss={onDismiss} />
+      <Chip
+        onClick={jest.fn()}
+        lead="Owner"
+        value="Bob"
+        onDismiss={onDismiss}
+      />
     );
     const dismissButton = wrapper.find(".p-chip__dismiss");
     dismissButton.simulate("click");
