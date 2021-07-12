16b423e0383bc2e247468c195052da5c30388b45
Jeff Pihach <jeff.pihach@canonical.com>
2021-07-12 12:27:40 -0600

    Add tests for invalid data to highlightSubString

--- src/utils.test.js
+++ src/utils.test.js
@@ -7,4 +7,18 @@ describe("highlightSubString function ", () => {
     const testString = highlightSubString(string, subString);
     expect(testString).toEqual({ match: true, text: "re<strong>act</strong>" });
   });
+
+  it("gracefully fails when invalid string is provided", () => {
+    expect(highlightSubString(undefined, "somesub")).toEqual({
+      text: "",
+      match: false,
+    });
+  });
+
+  it("gracefully fails when invalid substring is provided", () => {
+    expect(highlightSubString("somestring", undefined)).toEqual({
+      text: "somestring",
+      match: false,
+    });
+  });
 });
