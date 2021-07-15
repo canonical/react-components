49c7f2b89d03c259d985c9cd635842dbe6d42062
Caleb Ellis <caleb.ellis@canonical.com>
2021-07-15 16:34:28 +1000

    Migrate Textarea to TypeScript (#525)
    Co-authored-by: huwshimi <huwshimi@users.noreply.github.com>

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,14 +1,73 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import React, { useEffect, useRef } from "react";
+import type { HTMLAttributes, ReactNode } from "react";
 
 import Field from "../Field";
+
+/**
+ * The props for the Textarea component.
+ */
+export type Props = {
+  /**
+   * The content for caution validation.
+   */
+  caution?: ReactNode;
+  /**
+   * Optional class(es) to pass to the textarea element.
+   */
+  className?: string;
+  /**
+   * The content for error validation.
+   */
+  error?: ReactNode;
+  /**
+   * Whether the textarea should grow to fit the content automatically.
+   */
+  grow?: boolean;
+  /**
+   * Help text to show below the field.
+   */
+  help?: ReactNode;
+  /**
+   * The id of the textarea.
+   */
+  id?: string;
+  /**
+   * The label for the field.
+   */
+  label?: ReactNode;
+  /**
+   * Optional class(es) to pass to the label component.
+   */
+  labelClassName?: string;
+  /**
+   * Whether the field is required.
+   */
+  required?: boolean;
+  /**
+   * Whether the form field should have a stacked appearance.
+   */
+  stacked?: boolean;
+  /**
+   * The content for success validation.
+   */
+  success?: ReactNode;
+  /**
+   * Whether to focus on the input on initial render.
+   */
+  takeFocus?: boolean;
+  /**
+   * Optional class(es) to pass to the wrapping Field component
+   */
+  wrapperClassName?: string;
+} & HTMLAttributes<HTMLTextAreaElement>;
 
 const Textarea = ({
   caution,
   className,
   error,
-  grow,
+  grow = false,
   help,
   id,
   label,
@@ -18,10 +77,10 @@ const Textarea = ({
   stacked,
   style,
   success,
-  takeFocus,
+  takeFocus = false,
   wrapperClassName,
   ...props
-}) => {
+}: Props): JSX.Element => {
   const textareaRef = useRef(null);
 
   useEffect(() => {
@@ -47,6 +106,7 @@ const Textarea = ({
         className={classNames("p-form-validation__input", className)}
         id={id}
         onKeyUp={(evt) => {
+          onKeyUp && onKeyUp(evt);
           if (grow) {
             evt.currentTarget.style.height =
               evt.currentTarget.scrollHeight + "px";
@@ -83,9 +143,6 @@ Textarea.propTypes = {
   stacked: PropTypes.bool,
   style: PropTypes.object,
   success: PropTypes.node,
-  /**
-   * Focus on the textarea on first render.
-   */
   takeFocus: PropTypes.bool,
   wrapperClassName: PropTypes.string,
 };
