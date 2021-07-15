08b7493ef4b0d56909861e4174296cf2cd55c6dd
Huw Wilkins <huw.wilkins@canonical.com>
2021-07-15 16:50:59 +1000

    Migrate SearchBox to TypeScript.

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,8 +1,46 @@
 import classNames from "classnames";
 import React from "react";
 import PropTypes from "prop-types";
+import type { HTMLProps, FormEvent } from "react";
 
-const SearchBox = React.forwardRef(
+import Icon from "../Icon";
+
+export type Props = {
+  /**
+   * Whether autocomplete should be enabled for the search input.
+   */
+  autocomplete?: "on" | "off";
+  /**
+   * Optional classes to pass to the form element.
+   */
+  className?: string;
+  /**
+   * Whether the input and buttons should be disabled.
+   */
+  disabled?: boolean;
+  /**
+   * Whether the input value will be controlled via external state.
+   */
+  externallyControlled?: boolean;
+  /**
+   * A function that will be called when the input value changes.
+   */
+  onChange?: (inputValue: string) => void;
+  /**
+   * A function that will be called when the form is submitted.
+   */
+  onSubmit?: (evt: FormEvent) => void;
+  /**
+   * A search input placeholder message.
+   */
+  placeholder?: string;
+  /**
+   * The value of the search input when the state is externally controlled.
+   */
+  value?: string;
+} & HTMLProps<HTMLFormElement>;
+
+const SearchBox = React.forwardRef<HTMLFormElement, Props>(
   (
     {
       autocomplete = "on",
@@ -14,13 +52,15 @@ const SearchBox = React.forwardRef(
       placeholder = "Search",
       value,
       ...props
-    },
+    }: Props,
     ref
   ) => {
-    const input = React.createRef();
+    const input = React.createRef<HTMLInputElement | null>();
     const resetInput = () => {
       onChange && onChange("");
-      input.current.value = "";
+      if (input.current) {
+        input.current.value = "";
+      }
     };
     const submit = (e) => {
       e.preventDefault();
@@ -51,22 +91,20 @@ const SearchBox = React.forwardRef(
         />
         {value && (
           <button
-            alt="reset"
             className="p-search-box__reset"
             disabled={disabled}
             onClick={resetInput}
             type="reset"
           >
-            <i className="p-icon--close">Clear search field</i>
+            <Icon name="close">Clear search field</Icon>
           </button>
         )}
         <button
-          alt="search"
           className="p-search-box__button"
           disabled={disabled}
           type="submit"
         >
-          <i className="p-icon--search">Search</i>
+          <Icon name="search">Search</Icon>
         </button>
       </form>
     );
@@ -74,12 +112,9 @@ const SearchBox = React.forwardRef(
 );
 
 SearchBox.propTypes = {
-  autocomplete: PropTypes.string,
+  autocomplete: PropTypes.oneOf(["on", "off"]),
   className: PropTypes.string,
   disabled: PropTypes.bool,
-  /**
-   * Whether the input value will be controlled via external state.
-   */
   externallyControlled: PropTypes.bool,
   onChange: PropTypes.func,
   onSubmit: PropTypes.func,
