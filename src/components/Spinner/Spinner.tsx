fe348b08eda0b34cd37955afb5f225126c4fc6f7
Beth Collins <bethcollinsliv92@gmail.com>
2022-02-07 16:34:25 +0000

    Add aria attributes to spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -16,20 +16,42 @@ export type Props = {
    * Text to display next to the spinner.
    */
   text?: string;
+  /**
+   * What the role of the spinner should be.
+   */
+  role?: string;
+  /**
+   * The politeness setting of the spinner alert.
+   */
+  ariaLive: "assertive" | "off" | "polite";
+  /**
+   * Text hidden from view, read by screen readers.
+   */
+  labelText?: string;
 } & HTMLProps<HTMLSpanElement>;
 
 const Spinner = ({
   className,
   text,
   isLight = false,
+  ariaLive,
+  labelText,
+  role,
   ...props
 }: Props): JSX.Element => (
-  <span {...props} className={classNames(className, "p-text--default")}>
+  <span
+    {...props}
+    className={classNames(className, "p-text--default")}
+    role={role}
+    aria-live={ariaLive}
+  >
     <i
       className={classNames("p-icon--spinner", "u-animation--spin", {
         "is-light": isLight,
       })}
-    />
+    >
+      {labelText}
+    </i>
     {text && (
       <>
         &ensp;
