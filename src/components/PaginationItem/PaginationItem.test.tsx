937e89147910d7a5e7d027e015d374b9dfe52d6c
Beth Collins <bethcollinsliv92@gmail.com>
2022-03-22 16:30:33 +0000

    Add test for aria-current and update to testing library

--- src/components/PaginationItem/PaginationItem.test.tsx
+++ src/components/PaginationItem/PaginationItem.test.tsx
@@ -1,32 +1,43 @@
-import { shallow } from "enzyme";
 import React from "react";
+import { render } from "@testing-library/react";
 
 import PaginationItem from "./PaginationItem";
 
 describe("<PaginationItem />", () => {
   // snapshot tests
   it("renders and matches the snapshot", () => {
-    const component = shallow(
+    const { container } = render(
       <PaginationItem number={1} onClick={jest.fn()} />
     );
 
-    expect(component).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 
   // unit tests
   it("displays the page number", () => {
-    const component = shallow(
-      <PaginationItem number={1} onClick={jest.fn()} />
-    );
-
-    expect(component.find("button").text()).toEqual("1");
-  });
-
-  it("sets isActive", () => {
-    const component = shallow(
+    const { container } = render(
       <PaginationItem number={1} onClick={jest.fn()} isActive />
     );
 
-    expect(component.find("button").hasClass("is-active"));
+    expect(container.querySelector("button")).toHaveTextContent("1");
+  });
+
+  it("sets isActive", () => {
+    const { container } = render(
+      <PaginationItem number={1} onClick={jest.fn()} isActive />
+    );
+
+    expect(container.querySelector("button")).toHaveClass("is-active");
+  });
+
+  it("sets aria-current when isActive is true", () => {
+    const { container } = render(
+      <PaginationItem number={1} onClick={jest.fn()} isActive />
+    );
+
+    expect(container.querySelector("button")).toHaveAttribute(
+      "aria-current",
+      "page"
+    );
   });
 });
