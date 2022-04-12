7b59f051a8c26de10f1425b12ee02d7cb3501959
Peter Makowski <peter@makowski.uk>
2022-04-12 07:44:34 +0200

    update tooltip.spec.js

--- cypress/integration/tooltip.spec.js
+++ cypress/integration/tooltip.spec.js
@@ -4,8 +4,8 @@ context("Tooltip", () => {
   });
 
   it("clicking the child button element opens the tooltip", () => {
-    cy.findByRole("tooltip").should("not.exist");
+    cy.findByRole("tooltip").should("not.be.visible");
     cy.findByRole("button", { name: /Hover over me!/ }).click();
-    cy.findByRole("tooltip").should("exist");
+    cy.findByRole("tooltip").should("be.visible");
   });
 });
