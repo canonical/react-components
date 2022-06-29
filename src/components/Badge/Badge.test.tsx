6c72b2201cca416655ca16010d6ea2cabda304ea
ClementChaumel <clement.chaumel@gmail.com>
2022-06-29 12:16:32 +0200

    Add tests for rounding

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -20,3 +20,50 @@ it("can set its type as undefined large number", () => {
   // Check that the correct value is displayed.
   expect(screen.getByText("999+")).toBeInTheDocument();
 });
+
+it("rounds number correctly for thousands", () => {
+  render(<Badge value={1200} badgeType={BadgeType.ROUNDED_LARGE_NUMBER} />);
+  // Check that the correct value is displayed.
+  expect(screen.getByText("1.2k")).toBeInTheDocument();
+});
+
+it("rounds number correctly for millions", () => {
+  render(
+    <Badge value={132456455} badgeType={BadgeType.ROUNDED_LARGE_NUMBER} />
+  );
+  // Check that the correct value is displayed.
+  expect(screen.getByText("132M")).toBeInTheDocument();
+});
+
+it("rounds number correctly for billions", () => {
+  render(
+    <Badge value={13245645512} badgeType={BadgeType.ROUNDED_LARGE_NUMBER} />
+  );
+  // Check that the correct value is displayed.
+  expect(screen.getByText("13B")).toBeInTheDocument();
+});
+
+it("rounds number correctly for trillions", () => {
+  render(
+    <Badge value={132456455123112} badgeType={BadgeType.ROUNDED_LARGE_NUMBER} />
+  );
+  // Check that the correct value is displayed.
+  expect(screen.getByText("132T")).toBeInTheDocument();
+});
+
+it("displays the correct max value if it exceeds 999T", () => {
+  render(
+    <Badge
+      value={1324564551231125}
+      badgeType={BadgeType.ROUNDED_LARGE_NUMBER}
+    />
+  );
+  // Check that the correct value is displayed.
+  expect(screen.getByText("+999T")).toBeInTheDocument();
+});
+
+it("renders negative numbers as 0", () => {
+  render(<Badge value={-1} badgeType={BadgeType.UNDEFINED_LARGE_NUMBER} />);
+  // Check that the correct value is displayed.
+  expect(screen.getByText("0")).toBeInTheDocument();
+});
