81b6c300207f74cfc33006a97f29d40f8f0a3c58
Peter Makowski <peter@makowski.uk>
2022-08-25 13:19:44 +0200

    fix: Broken homepage link in NPM #619 - update all github urls to use canonical org

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -10,7 +10,7 @@ import { IS_DEV } from "../../utils";
 const Loader = (props: SpinnerProps): JSX.Element => {
   if (IS_DEV) {
     console.warn(
-      "The Loader component has been renamed to Spinner and will be removed in a future release. https://canonical-web-and-design.github.io/react-components/?path=/story/spinner--default-story"
+      "The Loader component has been renamed to Spinner and will be removed in a future release. https://canonical.github.io/react-components/?path=/story/spinner--default-story"
     );
   }
   return <Spinner {...props} />;
