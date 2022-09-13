bdaa69ac49cc2d5431dcca7cb8c263cf7a7c7eec
Huw Wilkins <huw.wilkins@canonical.com>
2022-09-13 14:40:47 +1000

    WIP

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,35 +1,33 @@
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Card from "./Card";
 
 describe("Card ", () => {
   it("renders", () => {
-    const wrapper = shallow(
-      <Card title="This is the title">Test content</Card>
-    );
-    expect(wrapper).toMatchSnapshot();
+    render(<Card title="This is the title">Test content</Card>);
+    expect(screen.get).toMatchSnapshot();
   });
 
   it("can display a header", () => {
-    const wrapper = shallow(<Card thumbnail="test.png"></Card>);
+    render(<Card thumbnail="test.png"></Card>);
     expect(wrapper.find(".p-card__thumbnail").prop("src")).toEqual("test.png");
   });
 
   it("can be highlighted", () => {
-    const wrapper = shallow(<Card highlighted={true}></Card>);
+    render(<Card highlighted={true}></Card>);
     expect(wrapper.prop("className").includes("p-card--highlighted")).toBe(
       true
     );
   });
 
   it("can be an overlay", () => {
-    const wrapper = shallow(<Card overlay={true}></Card>);
+    render(<Card overlay={true}></Card>);
     expect(wrapper.prop("className").includes("p-card--overlay")).toBe(true);
   });
 
   it("can add additional classes", () => {
-    const wrapper = shallow(<Card className="extra-class"></Card>);
+    render(<Card className="extra-class"></Card>);
     const className = wrapper.prop("className");
     expect(className.includes("p-card")).toBe(true);
     expect(className.includes("extra-class")).toBe(true);
