4dec254b9b05b1b42e619ab920186336bd582226
Huw Wilkins <huw.wilkins@canonical.com>
2022-09-14 09:30:57 +1000

    Migrate Col to RTL.

--- src/components/Col/Col.test.tsx
+++ src/components/Col/Col.test.tsx
@@ -1,72 +1,76 @@
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Col from "./Col";
 
 describe("Col ", () => {
   it("renders", () => {
-    const wrapper = shallow(<Col size={3}>Test content</Col>);
-    expect(wrapper).toMatchSnapshot();
+    render(
+      <Col data-testid="col" size={3}>
+        Test content
+      </Col>
+    );
+    expect(screen.getByTestId("col")).toMatchSnapshot();
   });
 
   it("can set a size for small screens", () => {
-    const wrapper = shallow(
-      <Col size={3} small={1}>
+    render(
+      <Col data-testid="col" size={3} small={1}>
         Test content
       </Col>
     );
-    expect(wrapper.prop("className").includes("col-small-1")).toBe(true);
+    expect(screen.getByTestId("col")).toHaveClass("col-small-1");
   });
 
   it("can set a size for medium screens", () => {
-    const wrapper = shallow(
-      <Col size={3} medium={1}>
+    render(
+      <Col data-testid="col" size={3} medium={1}>
         Test content
       </Col>
     );
-    expect(wrapper.prop("className").includes("col-medium-1")).toBe(true);
+    expect(screen.getByTestId("col")).toHaveClass("col-medium-1");
   });
 
   it("can set a size for large screens", () => {
-    const wrapper = shallow(
-      <Col size={3} large={1}>
+    render(
+      <Col data-testid="col" size={3} large={1}>
         Test content
       </Col>
     );
-    expect(wrapper.prop("className").includes("col-large-1")).toBe(true);
+    expect(screen.getByTestId("col")).toHaveClass("col-large-1");
   });
 
   it("can set empty cols for small screens", () => {
-    const wrapper = shallow(
-      <Col size={3} emptySmall={1}>
+    render(
+      <Col data-testid="col" size={3} emptySmall={1}>
         Test content
       </Col>
     );
-    expect(wrapper.prop("className").includes("col-start-small-1")).toBe(true);
+    expect(screen.getByTestId("col")).toHaveClass("col-start-small-1");
   });
 
   it("can set empty cols for medium screens", () => {
-    const wrapper = shallow(
-      <Col size={3} emptyMedium={1}>
+    render(
+      <Col data-testid="col" size={3} emptyMedium={1}>
         Test content
       </Col>
     );
-    expect(wrapper.prop("className").includes("col-start-medium-1")).toBe(true);
+    expect(screen.getByTestId("col")).toHaveClass("col-start-medium-1");
   });
 
   it("can set empty cols for large screens", () => {
-    const wrapper = shallow(
-      <Col size={3} emptyLarge={1}>
+    render(
+      <Col data-testid="col" size={3} emptyLarge={1}>
         Test content
       </Col>
     );
-    expect(wrapper.prop("className").includes("col-start-large-1")).toBe(true);
+    expect(screen.getByTestId("col")).toHaveClass("col-start-large-1");
   });
 
   it("can add additional classes", () => {
-    const wrapper = shallow(<Col size={3} className="extra-class"></Col>);
-    const className = wrapper.prop("className");
-    expect(className.includes("col-3")).toBe(true);
-    expect(className.includes("extra-class")).toBe(true);
+    render(<Col data-testid="col" size={3} className="extra-class"></Col>);
+    const col = screen.getByTestId("col");
+    expect(col).toHaveClass("col-3");
+    expect(col).toHaveClass("extra-class");
   });
 });
