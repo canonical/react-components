fc251f69a9c13b39a6988269b329b3aca12131bc
Huw Wilkins <huw.wilkins@canonical.com>
2022-09-13 14:55:32 +1000

    Migrate Card to RTL

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -6,30 +6,39 @@ import Card from "./Card";
 describe("Card ", () => {
   it("renders", () => {
     render(<Card title="This is the title">Test content</Card>);
-    expect(screen.get).toMatchSnapshot();
+    expect(screen.getByRole("group")).toMatchSnapshot();
+  });
+
+  it("can have a title", () => {
+    render(<Card title="This is the title">Test content</Card>);
+    expect(
+      screen.getByRole("group", { name: "This is the title" })
+    ).toBeInTheDocument();
   });
 
   it("can display a header", () => {
     render(<Card thumbnail="test.png"></Card>);
-    expect(wrapper.find(".p-card__thumbnail").prop("src")).toEqual("test.png");
+    // Find the visible image in the DOM.
+    // eslint-disable-next-line testing-library/no-node-access
+    const image = document.querySelector(".p-card__thumbnail");
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("src", "test.png");
   });
 
   it("can be highlighted", () => {
     render(<Card highlighted={true}></Card>);
-    expect(wrapper.prop("className").includes("p-card--highlighted")).toBe(
-      true
-    );
+    expect(screen.getByRole("group")).toHaveClass("p-card--highlighted");
   });
 
   it("can be an overlay", () => {
     render(<Card overlay={true}></Card>);
-    expect(wrapper.prop("className").includes("p-card--overlay")).toBe(true);
+    expect(screen.getByRole("group")).toHaveClass("p-card--overlay");
   });
 
   it("can add additional classes", () => {
     render(<Card className="extra-class"></Card>);
-    const className = wrapper.prop("className");
-    expect(className.includes("p-card")).toBe(true);
-    expect(className.includes("extra-class")).toBe(true);
+    const card = screen.getByRole("group");
+    expect(card).toHaveClass("p-card");
+    expect(card).toHaveClass("extra-class");
   });
 });
