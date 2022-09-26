c2c54a3c5f8db26a4ad10d252c42448b98084017
Huw Wilkins <huw.wilkins@canonical.com>
2022-09-26 10:57:44 +1000

    Use test id to select the chip.

--- src/components/Chip/Chip.test.tsx
+++ src/components/Chip/Chip.test.tsx
@@ -6,25 +6,25 @@ import userEvent from "@testing-library/user-event";
 
 describe("Chip ", () => {
   it("renders default chip", () => {
-    render(<Chip role="option" value="positive" />);
-    expect(screen.getByRole("option")).toMatchSnapshot();
+    render(<Chip data-testid="chip" value="positive" />);
+    expect(screen.getByTestId("chip")).toMatchSnapshot();
   });
 
   it("renders lead-value chip", () => {
-    render(<Chip role="option" lead="Owner" value="Bob" />);
-    expect(screen.getByRole("option")).toMatchSnapshot();
+    render(<Chip data-testid="chip" lead="Owner" value="Bob" />);
+    expect(screen.getByTestId("chip")).toMatchSnapshot();
   });
 
   it("displays the value", () => {
-    render(<Chip role="option" value="positive" />);
+    render(<Chip data-testid="chip" value="positive" />);
     expect(
-      within(screen.getByRole("option")).getByText("positive")
+      within(screen.getByTestId("chip")).getByText("positive")
     ).toBeInTheDocument();
   });
 
   it("displays the lead-value", () => {
-    render(<Chip role="option" lead="Owner" value="Bob" />);
-    const chip = screen.getByRole("option");
+    render(<Chip data-testid="chip" lead="Owner" value="Bob" />);
+    const chip = screen.getByTestId("chip");
     expect(within(chip).getByText("OWNER")).toBeInTheDocument(); // Colon and space added with CSS
     expect(within(chip).getByText("Bob")).toBeInTheDocument(); // Colon and space added with CSS
   });
@@ -32,10 +32,10 @@ describe("Chip ", () => {
   it("displays the dismiss action", () => {
     const onDismiss = jest.fn();
     render(
-      <Chip role="option" lead="Owner" value="Bob" onDismiss={onDismiss} />
+      <Chip data-testid="chip" lead="Owner" value="Bob" onDismiss={onDismiss} />
     );
     expect(
-      within(screen.getByRole("option")).getByRole("button", {
+      within(screen.getByTestId("chip")).getByRole("button", {
         name: Label.Dismiss,
       })
     ).toBeInTheDocument();
@@ -44,9 +44,9 @@ describe("Chip ", () => {
   it("calls onDismiss when clicked", async () => {
     const onDismiss = jest.fn();
     render(
-      <Chip role="option" lead="Owner" value="Bob" onDismiss={onDismiss} />
+      <Chip data-testid="chip" lead="Owner" value="Bob" onDismiss={onDismiss} />
     );
-    const dismissButton = within(screen.getByRole("option")).getByRole(
+    const dismissButton = within(screen.getByTestId("chip")).getByRole(
       "button",
       {
         name: Label.Dismiss,
@@ -58,81 +58,91 @@ describe("Chip ", () => {
 
   it("calls onKeyDown when key pressed", async () => {
     const onClick = jest.fn();
-    render(<Chip role="option" lead="Owner" value="Bob" onClick={onClick} />);
-    const chip = screen.getByRole("option");
+    render(
+      <Chip data-testid="chip" lead="Owner" value="Bob" onClick={onClick} />
+    );
+    const chip = screen.getByTestId("chip");
     await userEvent.type(chip, "{enter}");
     expect(onClick).toHaveBeenCalledWith({ lead: "Owner", value: "Bob" });
   });
 
   it("calls onClick when clicking on the chip", async () => {
     const onClick = jest.fn();
-    render(<Chip role="option" lead="Owner" value="Bob" onClick={onClick} />);
-    await userEvent.click(screen.getByRole("option"));
+    render(
+      <Chip data-testid="chip" lead="Owner" value="Bob" onClick={onClick} />
+    );
+    await userEvent.click(screen.getByTestId("chip"));
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
   it("renders positive chip", () => {
-    render(<Chip role="option" appearance="positive" value="positive" />);
-    expect(screen.getByRole("option")).toHaveClass("p-chip--positive");
+    render(<Chip data-testid="chip" appearance="positive" value="positive" />);
+    expect(screen.getByTestId("chip")).toHaveClass("p-chip--positive");
   });
 
   it("renders negative chip", () => {
-    render(<Chip role="option" appearance="negative" value="negative" />);
-    expect(screen.getByRole("option")).toHaveClass("p-chip--negative");
+    render(<Chip data-testid="chip" appearance="negative" value="negative" />);
+    expect(screen.getByTestId("chip")).toHaveClass("p-chip--negative");
   });
 
   it("renders caution chip", () => {
-    render(<Chip role="option" appearance="caution" value="caution" />);
-    expect(screen.getByRole("option")).toHaveClass("p-chip--caution");
+    render(<Chip data-testid="chip" appearance="caution" value="caution" />);
+    expect(screen.getByTestId("chip")).toHaveClass("p-chip--caution");
   });
 
   it("renders information chip", () => {
-    render(<Chip role="option" appearance="information" value="information" />);
-    expect(screen.getByRole("option")).toHaveClass("p-chip--information");
+    render(
+      <Chip data-testid="chip" appearance="information" value="information" />
+    );
+    expect(screen.getByTestId("chip")).toHaveClass("p-chip--information");
   });
 
   it("renders extra props", () => {
     render(
       <Chip
-        role="option"
+        data-testid="chip"
         appearance="information"
         lead="Owner"
-        onDismiss={jest.fn()}
         value="Bob"
-        data-testid="testID"
+        disabled
       />
     );
-    expect(screen.getByRole("option")).toHaveAttribute("data-testid", "testID");
+    expect(screen.getByTestId("chip")).toBeDisabled();
   });
 
   it("passes additional classes", () => {
     render(
-      <Chip role="option" className="extra-extra" lead="Owner" value="Bob" />
+      <Chip
+        data-testid="chip"
+        className="extra-extra"
+        lead="Owner"
+        value="Bob"
+      />
     );
-    expect(screen.getByRole("option")).toHaveClass("extra-extra");
+    expect(screen.getByTestId("chip")).toHaveClass("extra-extra");
   });
 
   it("passes additional classes to a dismissable chip", () => {
     render(
       <Chip
-        role="option"
+        data-testid="chip"
         className="extra-extra"
         lead="Owner"
         onDismiss={jest.fn()}
         value="Bob"
       />
     );
-    expect(screen.getByRole("option")).toHaveClass("extra-extra");
+    expect(screen.getByTestId("chip")).toHaveClass("extra-extra");
   });
 
   it("does not submit forms when clicking on the chip", async () => {
     const onSubmit = jest.fn();
     render(
       <form onSubmit={onSubmit}>
-        <Chip role="option" lead="Owner" value="Bob" />
+        <Chip data-testid="chip" lead="Owner" value="Bob" />
       </form>
     );
-    await userEvent.click(screen.getByRole("option"));
+    await userEvent.click(screen.getByTestId("chip"));
     expect(onSubmit).not.toHaveBeenCalled();
   });
 
@@ -140,10 +150,15 @@ describe("Chip ", () => {
     const onSubmit = jest.fn();
     render(
       <form onSubmit={onSubmit}>
-        <Chip role="option" lead="Owner" onDismiss={jest.fn()} value="Bob" />
+        <Chip
+          data-testid="chip"
+          lead="Owner"
+          onDismiss={jest.fn()}
+          value="Bob"
+        />
       </form>
     );
-    const dismissButton = within(screen.getByRole("option")).getByRole(
+    const dismissButton = within(screen.getByTestId("chip")).getByRole(
       "button",
       {
         name: Label.Dismiss,
