a10db65740833c1eb6a02099de24f4218dd98b8c
ClementChaumel <clement.chaumel@gmail.com>
2023-02-14 11:10:52 +0100

    Rmeove dupplicate imports

--- src/components/SearchBox/SearchBox.test.tsx
+++ src/components/SearchBox/SearchBox.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
-import userEvent from "@testing-library/user-event";
-import { render, screen } from "@testing-library/react";
 import SearchBox, { Label } from "./SearchBox";
 
 describe("SearchBox ", () => {
