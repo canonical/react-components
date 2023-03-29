aaad61357d14e086324fa7254adba69ac216e31f
ClementChaumel <clement.chaumel@gmail.com>
2023-03-29 09:44:22 +0200

    Fix e2e tests for tooltip

--- cypress/e2e/tooltip.cy.js
+++ cypress/e2e/tooltip.cy.js
@@ -4,39 +4,39 @@ context("Tooltip", () => {
   });
 
   it("clicking the child button element opens the tooltip", () => {
-    cy.findByRole("tooltip").should("not.be.visible");
+    cy.findByRole("tooltip").should("not.exist");
     cy.findByRole("button", { name: /Hover over me!/ }).click();
     cy.findByRole("tooltip").should("be.visible");
   });
 
   it("hovering the child button element opens the tooltip", () => {
-    cy.findByRole("tooltip").should("not.be.visible");
+    cy.findByRole("tooltip").should("not.exist");
     cy.findByRole("button", { name: /Hover over me!/ }).trigger("mouseover");
     cy.findByRole("tooltip").should("be.visible");
   });
 
   it("when activated on click tooltip remains open when the cursor is moved outside", () => {
-    cy.findByRole("tooltip").should("not.be.visible");
+    cy.findByRole("tooltip").should("not.exist");
     cy.findByRole("button", { name: /Hover over me!/ }).click();
     cy.get("body").trigger("mousemove", { clientX: 0, clientY: 0 });
     cy.findByRole("tooltip").should("be.visible");
     cy.get("body").type("{esc}");
-    cy.findByRole("tooltip").should("not.be.visible");
+    cy.findByRole("tooltip").should("not.exist");
   });
 
   it("when activated on click tooltip can be closed with an ESC key", () => {
-    cy.findByRole("tooltip").should("not.be.visible");
+    cy.findByRole("tooltip").should("not.exist");
     cy.findByRole("button", { name: /Hover over me!/ }).click();
     cy.findByRole("tooltip").should("be.visible");
     cy.get("body").type("{esc}");
-    cy.findByRole("tooltip").should("not.be.visible");
+    cy.findByRole("tooltip").should("not.exist");
   });
 
   it("when activated on click tooltip can be closed by clicking outside of it", () => {
-    cy.findByRole("tooltip").should("not.be.visible");
+    cy.findByRole("tooltip").should("not.exist");
     cy.findByRole("button", { name: /Hover over me!/ }).click();
     cy.findByRole("tooltip").should("be.visible");
     cy.get("body").click(0, 0);
-    cy.findByRole("tooltip").should("not.be.visible");
+    cy.findByRole("tooltip").should("not.exist");
   });
 });
