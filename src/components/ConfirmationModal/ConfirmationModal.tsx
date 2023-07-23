464e65ea6c8a166877c96144718ebb10c5d0932e
britneywwc <britneywwc@gmail.com>
2023-07-23 12:07:42 +0800

    Allow ConfirmationModal to focus on positive confirm button#945

--- src/components/ConfirmationModal/ConfirmationModal.tsx
+++ src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -1,5 +1,5 @@
-import React, { MouseEvent, ReactElement } from "react";
-import type { ReactNode } from "react";
+import React, { MouseEvent, ReactElement, useRef, useEffect } from "react";
+import type { ReactNode, MutableRefObject, RefObject } from "react";
 import { PropsWithSpread, ValueOf } from "types";
 import Button, { ButtonAppearance } from "components/Button";
 import Modal, { ModalProps } from "components/Modal";
@@ -43,27 +43,50 @@ export const ConfirmationModal = ({
   onConfirm,
   ...props
 }: Props): ReactElement => {
-  return (
-    <Modal
-      buttonRow={
-        <>
-          {confirmExtra}
-          <Button className="u-no-margin--bottom" onClick={props.close}>
-            {cancelButtonLabel}
-          </Button>
-          <Button
-            appearance={confirmButtonAppearance}
-            className="u-no-margin--bottom"
-            onClick={onConfirm}
-          >
-            {confirmButtonLabel}
-          </Button>
-        </>
+  const modalRef: MutableRefObject<HTMLElement> = useRef(null);
+
+  useEffect(() => {
+    if (modalRef.current) {
+      const positiveButton: HTMLButtonElement | null =
+        modalRef?.current?.querySelector("button.p-button--positive");
+      // If the modal has a positive button then focus on that after opening
+      // to make a better keyboard navigation experience.
+      if (positiveButton) {
+        console.log("positive button focus");
+        positiveButton.focus();
+      } else {
+        // If there is no button then focus on the modal wrapper.
+        modalRef.current.focus();
       }
-      {...props}
+    }
+  }, []);
+
+  return (
+    <div
+      className="p-confirmation-modal"
+      ref={modalRef as RefObject<HTMLDivElement>}
     >
-      {children}
-    </Modal>
+      <Modal
+        buttonRow={
+          <>
+            {confirmExtra}
+            <Button className="u-no-margin--bottom" onClick={props.close}>
+              {cancelButtonLabel}
+            </Button>
+            <Button
+              appearance={confirmButtonAppearance}
+              className="u-no-margin--bottom"
+              onClick={onConfirm}
+            >
+              {confirmButtonLabel}
+            </Button>
+          </>
+        }
+        {...props}
+      >
+        {children}
+      </Modal>
+    </div>
   );
 };
 
