3f849bf78b7003bca795e3978d818ca1b9b945ac
Peter Makowski <me+git@petermakowski.io>
2023-11-23 08:55:27 +0100

    docs: upgrade storybook to v7

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,6 @@
 import { themes } from "@storybook/theming";
 import "vanilla-framework/scss/build.scss";
 
-// or global addParameters
 export const parameters = {
   docs: {
     theme: themes.vanillaish,
--- cypress/e2e/accordion.cy.js
+++ cypress/e2e/accordion.cy.js
@@ -7,21 +7,23 @@ describe("Storybook", () => {
   context("Accordion", () => {
     it("should show items when clicked", () => {
       cy.get("button").contains("Advanced topics").click();
-      cy.get("#root > aside > ul > li > section > p:first").should(
-        "be.visible"
-      );
-      cy.get("#root > aside > ul > li > section > p:first").should(
-        "have.text",
-        "Charm bundles"
-      );
+      cy.findByRole("tabpanel", { name: "Advanced topics" }).within(() => {
+        cy.get("p:first")
+          .should("have.text", "Charm bundles")
+          .should("be.visible");
+      });
     });
 
     it("should hide items when clicked again", () => {
       cy.get("button").contains("Advanced topics").click();
+
+      cy.findByRole("tabpanel", { name: "Advanced topics" }).should(
+        "be.visible"
+      );
       cy.get("button").contains("Advanced topics").click();
 
-      cy.get("#root > aside > ul > li > section > p:first").should(
-        "not.be.visible"
+      cy.findByRole("tabpanel", { name: "Advanced topics" }).should(
+        "not.exist"
       );
     });
   });
