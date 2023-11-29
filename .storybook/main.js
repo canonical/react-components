a98449ee6598aa4b0ed8b3dee88d546551531737
Peter Makowski <peter.makowski@canonical.com>
2023-11-29 15:55:37 +0100

    docs: upgrade storybook to v7 (#995)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,15 @@
 const path = require("path");
 
-module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
+const config = {
   stories: ["../src/**/*.stories.@(js|ts|jsx|tsx|mdx)"],
   addons: [
+    "@storybook/addon-essentials",
     "@storybook/addon-docs",
     "@storybook/addon-controls",
     "@storybook/addon-a11y",
+    "@storybook/addon-mdx-gfm",
   ],
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
@@ -18,4 +17,16 @@ module.exports = {
     });
     return config;
   },
+  docs: {
+    autodocs: "tag",
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  features: {
+    storyStoreV7: false,
+  },
 };
+
+export default config;
