ae9af3aa5a06c6474286519393bda19e73105c28
Rafael Melo <rsmelo_@hotmail.com>
2024-01-07 13:50:18 -0300

    refactor: migrate from MDX to CSF stories with autodocs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,18 @@
-import { themes } from "@storybook/theming";
 import "vanilla-framework/scss/build.scss";
+
+import { themes } from "@storybook/theming";
+import { Title, Description, ArgTypes, Stories } from '@storybook/blocks';
 
 export const parameters = {
   docs: {
     theme: themes.vanillaish,
+    page: () => (
+      <>
+        <Title />
+        <Description />
+        <ArgTypes sort="alpha" />
+        <Stories />
+      </>
+    ),
   },
 };
---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import type { Meta } from "@storybook/react";
+
+import Card from "./";
+import Col from "../Col";
+import Row from "../Row";
+
+const meta: Meta<typeof Card> = {
+  title: "Card",
+  component: Card,
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: `This is a [React](https://reactjs.org/) component for the Vanilla [Card](https://docs.vanillaframework.io/patterns/card/). 
+        <br/>
+        <br/>
+        There are four card styles available to use in Vanilla: default, header, highlighted and overlay. Our card component will expand to fill the full width of its parent container.`,
+      },
+    },
+  },
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    title: "We'd love to have you join us as a partner.",
+    children:
+      "If you are an independent software vendor or bundle author, it's easy to apply. You can find out more below.",
+  },
+};
+
+export const Header = {
+  args: {
+    title: "Raspberry Pi2 and Pi3",
+    thumbnail: "https://assets.ubuntu.com/v1/dca2e4c4-raspberry-logo.png",
+    children:
+      "For fun, for education and for profit, the RPi makes device development personal and entertaining. With support for both the Pi2 and the new Pi3, Ubuntu Core supports the world’s most beloved board.",
+  },
+};
+
+export const Highlighted = {
+  args: {
+    ...Default.args,
+    highlighted: true,
+  },
+};
+
+export const Overlay = {
+  render: () => (
+    <section
+      className="p-strip--image is-light"
+      style={{
+        backgroundImage:
+          "url('https://assets.ubuntu.com/v1/0a98afcd-screenshot_desktop.jpg')",
+      }}
+    >
+      <Row>
+        <Col size={6} emptyLarge={7}>
+          <Card title="Web browsing" overlay>
+            Renowned for speed and security, Ubuntu and Firefox make browsing
+            the web a pleasure again. Ubuntu also includes Chrome, Opera and
+            other browsers that can be installed from the Ubuntu Software
+            Centre.
+          </Card>
+        </Col>
+      </Row>
+    </section>
+  ),
+};
