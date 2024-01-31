78f88d75b23e95203e2e17ad0bc97ebfc898ed74
Mason Hu <mason.hu@canonical.com>
2024-01-31 18:11:40 +0200

    feat: implement externally controlled mode for TablePagination
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- src/components/TablePagination/TablePagination.tsx
+++ src/components/TablePagination/TablePagination.tsx
@@ -1,59 +1,19 @@
 import React, {
-  ChangeEvent,
-  Children,
   HTMLAttributes,
   PropsWithChildren,
-  ReactElement,
   ReactNode,
-  RefObject,
-  cloneElement,
-  useEffect,
-  useRef,
   useState,
 } from "react";
-import classnames from "classnames";
-import { usePagination } from "hooks";
-import Select from "components/Select";
-import TablePaginationControls from "./TablePaginationControls";
 import "./TablePagination.scss";
+import TablePaginationControls from "./TablePaginationControls";
+import {
+  DEFAULT_PAGE_LIMITS,
+  generatePagingOptions,
+  renderChildren,
+} from "./utils";
+import { usePagination } from "hooks";
 
-/**
- * Determine if we are working with a small screen.
- * 'small screen' in this case is relative to the width of the description div
- */
-const figureSmallScreen = (descriptionRef: RefObject<HTMLDivElement>) => {
-  const descriptionElement = descriptionRef.current;
-  if (!descriptionElement) {
-    return true;
-  }
-  return descriptionElement.getBoundingClientRect().width < 230;
-};
-
-/**
- * Iterate direct react child components and override the value of the prop specified by @param dataForwardProp
- * for those child components.
- * @param children - react node children to iterate
- * @param dataForwardProp - the name of the prop from the children components to override
- * @param data - actual data to be passed to the prop specified by @param dataForwardProp
- */
-const renderChildren = (
-  children: ReactNode,
-  dataForwardProp: string,
-  data: unknown[]
-) => {
-  return Children.map(children, (child) => {
-    return cloneElement(child as ReactElement, {
-      [dataForwardProp]: data,
-    });
-  });
-};
-
-const DEFAULT_PAGE_LIMITS = [50, 100, 200];
-const generatePagingOptions = (pageLimits: number[]) => {
-  return pageLimits.map((limit) => ({ value: limit, label: `${limit}/page` }));
-};
-
-export type Props = PropsWithChildren<{
+export type BasePaginationProps = {
   /**
    * list of data elements to be paginated. This component is un-opinionated about
    * the structure of the data but it should be identical to the data structure
@@ -62,7 +22,7 @@ export type Props = PropsWithChildren<{
   data: unknown[];
   /**
    * prop name of the child table component that receives paginated data.
-   * default value is set to @constant rows, which is the data prop for the @func MainTable component
+   * default value is set to `rows`, which is the data prop for the `MainTable` component
    */
   dataForwardProp?: string;
   /**
@@ -85,94 +45,150 @@ export type Props = PropsWithChildren<{
    * place the pagination component above or below the table?
    */
   position?: "above" | "below";
-}> &
+};
+
+export type ExternalControlProps = BasePaginationProps & {
+  /**
+   * Whether the component will be controlled via external state.
+   */
+  externallyControlled?: true;
+  /**
+   * the total number of items available within the data. This prop is only relevant
+   * and will be required if `externallyControlled` is set to `true`.
+   */
+  totalItems: number;
+  /**
+   * the current page that's showing. This prop is only relevant and will be required
+   * if `externallyControlled` is set to `true`.
+   */
+  currentPage: number;
+  /**
+   * size per page. This prop is only relevant and will be required if
+   * `externallyControlled` is set to `true`.
+   */
+  pageSize: number;
+  /**
+   * callback indicating a page change event to the parent component.
+   * This prop is only relevant and will be required if `externallyControlled` is set
+   * to `true`.
+   */
+  onPageChange: (page: number) => void;
+  /**
+   * callback indicating a page size change event to the parent component.
+   * This prop is only relevant and will be required if `externallyControlled` is set
+   * to `true`.
+   */
+  onPageSizeChange: (pageSize: number) => void;
+};
+
+export type InternalControlProps = BasePaginationProps & {
+  /**
+   * Whether the component will be controlled via external state.
+   */
+  externallyControlled?: false;
+};
+
+export type Props = PropsWithChildren<
+  ExternalControlProps | InternalControlProps
+> &
   HTMLAttributes<HTMLDivElement>;
 
-const TablePagination = ({
-  data,
-  className,
-  itemName = "item",
-  description,
-  position = "above",
-  dataForwardProp = "rows",
-  pageLimits = DEFAULT_PAGE_LIMITS,
-  children,
-  ...divProps
-}: Props) => {
-  const descriptionRef = useRef<HTMLDivElement>(null);
-  const [isSmallScreen, setSmallScreen] = useState(false);
-  const [pageSize, setPageSize] = useState(() => {
+const TablePagination = (props: Props) => {
+  const {
+    data,
+    dataForwardProp = "rows",
+    itemName = "item",
+    className,
+    description,
+    pageLimits = DEFAULT_PAGE_LIMITS,
+    position = "above",
+    externallyControlled,
+    children,
+    ...divProps
+  } = props;
+
+  // Safety check to ensure pageSize is a valid option in
+  // pageLimits if the component is externally controlled
+  if (externallyControlled) {
+    let pageSizeFound = false;
+    for (const limit of pageLimits) {
+      if (limit === Number(props.pageSize)) {
+        pageSizeFound = true;
+        break;
+      }
+    }
+
+    if (!pageSizeFound) {
+      throw new Error(
+        `pageSize must be a valid option in pageLimits, pageLimits is set to [${pageLimits}]`
+      );
+    }
+  }
+
+  const [internalPageSize, setInternalPageSize] = useState(() => {
     return generatePagingOptions(pageLimits)[0].value;
   });
-  const { paginate, currentPage, pageData, totalItems } = usePagination(data, {
-    itemsPerPage: pageSize,
+  const {
+    paginate,
+    currentPage: internalCurrentPage,
+    pageData: internalData,
+  } = usePagination(externallyControlled ? [] : data, {
+    itemsPerPage: internalPageSize,
     autoResetPage: true,
   });
 
-  useEffect(() => {
-    const handleResize = () => {
-      setSmallScreen(figureSmallScreen(descriptionRef));
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [isSmallScreen]);
+  const controlData = externallyControlled ? data : internalData;
+  const controlPageSize = externallyControlled
+    ? props.pageSize
+    : internalPageSize;
+  const controlTotalItems = externallyControlled
+    ? props.totalItems
+    : data.length;
+  const controlCurrentPage = externallyControlled
+    ? props.currentPage
+    : internalCurrentPage;
 
-  const handlePageSizeChange = (e: ChangeEvent<HTMLSelectElement>) => {
+  const handlePageChange = (page: number) => {
+    if (externallyControlled) {
+      props.onPageChange(page);
+      return;
+    }
+
+    paginate(page);
+  };
+
+  const handlePageSizeChange = (pageSize: number) => {
+    if (externallyControlled) {
+      props.onPageSizeChange(pageSize);
+      return;
+    }
+
     paginate(1);
-    setPageSize(parseInt(e.target.value));
+    setInternalPageSize(pageSize);
   };
 
-  const getDescription = () => {
-    if (description) {
-      return description;
-    }
+  const clonedChildren = renderChildren(children, dataForwardProp, controlData);
+  const controls = (
+    <TablePaginationControls
+      {...divProps}
+      data={controlData}
+      className={className}
+      itemName={itemName}
+      description={description}
+      pageLimits={pageLimits}
+      totalItems={controlTotalItems}
+      currentPage={controlCurrentPage}
+      pageSize={controlPageSize}
+      onPageChange={handlePageChange}
+      onPageSizeChange={handlePageSizeChange}
+    />
+  );
 
-    const visibleCount = pageData.length;
-
-    if (isSmallScreen) {
-      return `${visibleCount} out of ${totalItems}`;
-    }
-
-    if (visibleCount === totalItems && visibleCount > 1) {
-      return `Showing all ${totalItems} ${itemName}s`;
-    }
-
-    return `Showing ${visibleCount} out of ${totalItems} ${itemName}${
-      totalItems !== 1 ? "s" : ""
-    }`;
-  };
-
-  const totalPages = Math.ceil(data.length / pageSize);
-  const clonedChildren = renderChildren(children, dataForwardProp, pageData);
   return (
     <>
-      {position === "below" && clonedChildren}
-      <div
-        className={classnames("pagination", className)}
-        {...divProps}
-        role="navigation"
-      >
-        <div className="description" ref={descriptionRef}>
-          {getDescription()}
-        </div>
-        <TablePaginationControls
-          onPageChange={paginate}
-          currentPage={currentPage}
-          totalPages={totalPages}
-        />
-        <Select
-          className="items-per-page"
-          label="Items per page"
-          labelClassName="u-off-screen"
-          id="itemsPerPage"
-          options={generatePagingOptions(pageLimits)}
-          onChange={handlePageSizeChange}
-          value={pageSize}
-        />
-      </div>
-      {position === "above" && clonedChildren}
+      {position === "above" && controls}
+      {clonedChildren}
+      {position === "below" && controls}
     </>
   );
 };
--- src/components/TablePagination/TablePaginationControls/TablePaginationControls.test.tsx
+++ src/components/TablePagination/TablePaginationControls/TablePaginationControls.test.tsx
@@ -4,14 +4,27 @@ import { render, screen } from "@testing-library/react";
 
 import TablePaginationControls from "./TablePaginationControls";
 
+const dummyData = [
+  { id: "row-1" },
+  { id: "row-2" },
+  { id: "row-3" },
+  { id: "row-4" },
+  { id: "row-5" },
+];
+
 describe("<TablePaginationControls />", () => {
   // snapshot tests
   it("renders table pagination controls and matches the snapshot", () => {
     render(
       <TablePaginationControls
+        data={dummyData}
+        itemName="item"
+        pageLimits={[20, 50, 100]}
+        totalItems={100}
+        currentPage={0}
+        pageSize={20}
         onPageChange={jest.fn()}
-        currentPage={1}
-        totalPages={50}
+        onPageSizeChange={jest.fn()}
       />
     );
 
