d13e8a23c12e95e6fd5eac39b685f1fdcd837e01
Huw Wilkins <huw.wilkins@canonical.com>
2024-08-26 15:52:34 +1000

    refactor: update TablePaginationControls to be used outside of TablePagination

--- src/components/TablePagination/TablePaginationControls/TablePaginationControls.test.tsx
+++ src/components/TablePagination/TablePaginationControls/TablePaginationControls.test.tsx
@@ -3,21 +3,15 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 
 import TablePaginationControls from "./TablePaginationControls";
-
-const dummyData = [
-  { id: "row-1" },
-  { id: "row-2" },
-  { id: "row-3" },
-  { id: "row-4" },
-  { id: "row-5" },
-];
+import userEvent from "@testing-library/user-event";
+import { Label } from "./TablePaginationControls";
 
 describe("<TablePaginationControls />", () => {
   // snapshot tests
   it("renders table pagination controls and matches the snapshot", () => {
     render(
       <TablePaginationControls
-        data={dummyData}
+        visibleCount={5}
         itemName="item"
         pageLimits={[20, 50, 100]}
         totalItems={100}
@@ -30,5 +24,134 @@ describe("<TablePaginationControls />", () => {
 
     expect(screen.getAllByRole("button")).toMatchSnapshot();
     expect(screen.getByRole("spinbutton")).toMatchSnapshot();
+  });
+
+  it("can go to the next page", async () => {
+    const onPageChange = jest.fn();
+    const onNextPage = jest.fn();
+    render(
+      <TablePaginationControls
+        visibleCount={5}
+        itemName="item"
+        pageLimits={[20, 50, 100]}
+        totalItems={100}
+        currentPage={2}
+        pageSize={20}
+        onPageChange={onPageChange}
+        onNextPage={onNextPage}
+        onPageSizeChange={jest.fn()}
+      />
+    );
+    await userEvent.click(
+      screen.getByRole("button", { name: Label.NEXT_PAGE })
+    );
+    expect(onPageChange).toHaveBeenCalledWith(3);
+    expect(onNextPage).toHaveBeenCalledWith(3);
+  });
+
+  it("can go to the previous page", async () => {
+    const onPageChange = jest.fn();
+    const onPreviousPage = jest.fn();
+    render(
+      <TablePaginationControls
+        visibleCount={5}
+        itemName="item"
+        pageLimits={[20, 50, 100]}
+        totalItems={100}
+        currentPage={2}
+        pageSize={20}
+        onPageChange={onPageChange}
+        onPreviousPage={onPreviousPage}
+        onPageSizeChange={jest.fn()}
+      />
+    );
+    await userEvent.click(
+      screen.getByRole("button", { name: Label.PREVIOUS_PAGE })
+    );
+    expect(onPageChange).toHaveBeenCalledWith(1);
+    expect(onPreviousPage).toHaveBeenCalledWith(1);
+  });
+
+  it("can set the page using the input", async () => {
+    const onInputPageChange = jest.fn();
+    render(
+      <TablePaginationControls
+        visibleCount={5}
+        itemName="item"
+        pageLimits={[20, 50, 100]}
+        totalItems={100}
+        currentPage={2}
+        pageSize={20}
+        onPageChange={jest.fn()}
+        onPreviousPage={jest.fn()}
+        onPageSizeChange={jest.fn()}
+        onInputPageChange={onInputPageChange}
+      />
+    );
+    await userEvent.type(
+      screen.getByRole("spinbutton", { name: Label.PAGE_NUMBER }),
+      "1"
+    );
+    expect(onInputPageChange).toHaveBeenCalledWith(21);
+  });
+
+  it("can hide the page input", async () => {
+    render(
+      <TablePaginationControls
+        visibleCount={5}
+        itemName="item"
+        pageLimits={[20, 50, 100]}
+        totalItems={100}
+        currentPage={2}
+        pageSize={20}
+        onPageChange={jest.fn()}
+        onPreviousPage={jest.fn()}
+        onPageSizeChange={jest.fn()}
+        showPageInput={false}
+      />
+    );
+    expect(
+      screen.queryByRole("spinbutton", { name: Label.PAGE_NUMBER })
+    ).not.toBeInTheDocument();
+  });
+
+  it("can display the description", async () => {
+    render(
+      <TablePaginationControls
+        visibleCount={5}
+        itemName="item"
+        pageLimits={[20, 50, 100]}
+        totalItems={100}
+        currentPage={2}
+        pageSize={20}
+        onPageChange={jest.fn()}
+        onPreviousPage={jest.fn()}
+        onPageSizeChange={jest.fn()}
+        showPageInput={false}
+      />
+    );
+    expect(document.querySelector(".description")?.textContent).toBe(
+      "Showing 5 out of 100 items"
+    );
+  });
+
+  it("can hide the description", async () => {
+    render(
+      <TablePaginationControls
+        visibleCount={5}
+        itemName="item"
+        pageLimits={[20, 50, 100]}
+        totalItems={100}
+        currentPage={2}
+        pageSize={20}
+        onPageChange={jest.fn()}
+        onPreviousPage={jest.fn()}
+        onPageSizeChange={jest.fn()}
+        displayDescription={false}
+      />
+    );
+    expect(
+      document.querySelector(".description")?.textContent
+    ).not.toBeUndefined();
   });
 });
