7f99f5a149911aff12adaca1436b70139af756b1
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2024-09-09 15:38:26 +0200

    fix tests

---
+++ babel.config.js
@@ -0,0 +1,69 @@
+const commonJs = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "babel-plugin-typescript-to-proptypes",
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          enums: "./src/enums",
+          components: "./src/components",
+          hooks: "./src/hooks",
+          types: "./src/types",
+          utils: "./src/utils",
+        },
+      },
+    ],
+  ],
+};
+const esm = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: false,
+      },
+    ],
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "babel-plugin-typescript-to-proptypes",
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          enums: "./src/enums",
+          components: "./src/components",
+          hooks: "./src/hooks",
+          types: "./src/types",
+          utils: "./src/utils",
+        },
+      },
+    ],
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        useEsModules: true,
+        corejs: 3,
+        helpers: false,
+      },
+    ],
+  ],
+};
+
+module.exports = {
+  env: {
+    cjs: commonJs,
+    test: commonJs,
+    esm,
+  },
+};
