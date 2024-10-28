2a0c9ecfe16bb8d346fca20f193bb2b4eb802f7d
Mykola Ptushchuk <mykola.ptushchuk@whykingz.com>
2024-10-28 17:21:52 +0200

    Theming support for stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,16 @@
 import { themes } from "@storybook/theming";
 import "vanilla-framework/scss/build.scss";
+import { themeType, WithThemeProvider } from "./addons/withTheme";
 
 export const parameters = {
   docs: {
     theme: themes.vanillaish,
   },
+  backgrounds: {
+    disable: true,
+  },
 };
+
+export const decorators = [WithThemeProvider];
+
+export const globalTypes = themeType;
