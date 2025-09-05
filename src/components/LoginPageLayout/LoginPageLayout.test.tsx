1054e1f4733925c2bf1106c41c0a6f472f79a8b5
Ethan Shaw <ethan.shaw@canonical.com>
2025-09-05 07:09:48 -0700

    fix(`LoginPageLayout`): no longer sets theme to `is-paper` (#1251)
    BREAKING CHANGE: LoginPageLayout no longer sets theme to is-paper

--- src/components/LoginPageLayout/LoginPageLayout.test.tsx
+++ src/components/LoginPageLayout/LoginPageLayout.test.tsx
@@ -1,8 +1,7 @@
-import React, { useLayoutEffect, useState } from "react";
+import React from "react";
 import { render, screen, within } from "@testing-library/react";
 
 import LoginPageLayout from "./LoginPageLayout";
-import userEvent from "@testing-library/user-event";
 
 it("should display the default logo", () => {
   render(<LoginPageLayout title="Login page" />);
@@ -32,55 +31,4 @@ it("should display the title", () => {
   expect(
     screen.getByRole("heading", { name: "Login page" }),
   ).toBeInTheDocument();
-});
-
-it("should add and then remove is-paper class to body if initially is-paper wasn't present", async () => {
-  const NoInitialIsPaperComponent = () => {
-    const [displayLogin, setDisplayLogin] = useState(false);
-    return (
-      <div>
-        {displayLogin ? (
-          <>
-            <LoginPageLayout title="Login page" />
-            <button onClick={() => setDisplayLogin(false)}>Remove login</button>
-          </>
-        ) : (
-          <button onClick={() => setDisplayLogin(true)}>Display login</button>
-        )}
-      </div>
-    );
-  };
-  render(<NoInitialIsPaperComponent />);
-  expect(document.querySelector("body")).not.toHaveClass("is-paper");
-  await userEvent.click(screen.getByRole("button", { name: "Display login" }));
-  expect(document.querySelector("body")).toHaveClass("is-paper");
-  await userEvent.click(screen.getByRole("button", { name: "Remove login" }));
-  expect(document.querySelector("body")).not.toHaveClass("is-paper");
-});
-
-it("shouldn't remove is-paper class to body if initially is-paper was present", async () => {
-  const InitialIsPaperComponent = () => {
-    const [displayLogin, setDisplayLogin] = useState(false);
-    useLayoutEffect(() => {
-      document.querySelector("body")?.classList.add("is-paper");
-    }, []);
-    return (
-      <div>
-        {displayLogin ? (
-          <>
-            <LoginPageLayout title="Login page" />
-            <button onClick={() => setDisplayLogin(false)}>Remove login</button>
-          </>
-        ) : (
-          <button onClick={() => setDisplayLogin(true)}>Display login</button>
-        )}
-      </div>
-    );
-  };
-  render(<InitialIsPaperComponent />);
-  expect(document.querySelector("body")).toHaveClass("is-paper");
-  await userEvent.click(screen.getByRole("button", { name: "Display login" }));
-  expect(document.querySelector("body")).toHaveClass("is-paper");
-  await userEvent.click(screen.getByRole("button", { name: "Remove login" }));
-  expect(document.querySelector("body")).toHaveClass("is-paper");
 });
