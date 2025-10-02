3901cc12d8219535854f6c4d3cbdcb85210fc90b
Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>
2025-10-02 18:27:40 +0200

    feat(CustomSelect): add selectedLabel
    Signed-off-by: Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>

--- src/components/CustomSelect/CustomSelect.test.tsx
+++ src/components/CustomSelect/CustomSelect.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, within } from "@testing-library/react";
 import CustomSelect from "./CustomSelect";
 import { CustomSelectOption } from "./CustomSelectDropdown";
 import React from "react";
@@ -11,6 +11,18 @@ describe("CustomSelect", () => {
   const options: CustomSelectOption[] = [
     { value: "1", label: "Option 1" },
     { value: "2", label: "Option 2" },
+    {
+      value: "3",
+      label: "Option 3",
+      selectedLabel: "Selected label of 3",
+    },
+    {
+      value: "4",
+      label: "Option 4",
+      selectedLabel: (
+        <span data-testid="selected-label-test-id">🇯🇵 Selected label of 4</span>
+      ),
+    },
   ];
 
   afterEach(() => {
@@ -68,5 +80,53 @@ describe("CustomSelect", () => {
     expect(mockOnChange).toHaveBeenCalledWith("2");
     const option = screen.queryByRole("option", { name: "Option 2" });
     expect(option).not.toBeInTheDocument();
+  });
+
+  it("should display the default placeholder when no option is selected", () => {
+    render(<CustomSelect options={options} value={null} onChange={() => {}} />);
+    expect(screen.getByText("Select an option")).toBeInTheDocument();
+  });
+
+  it("should display the standard label when selected option has no selectedLabel", () => {
+    render(<CustomSelect options={options} value="1" onChange={() => {}} />);
+    expect(screen.getByText("Option 1")).toBeInTheDocument();
+  });
+
+  it("should display the selectedLabel when the selected option has one", () => {
+    render(<CustomSelect options={options} value="3" onChange={() => {}} />);
+    expect(screen.getByText("Selected label of 3")).toBeInTheDocument();
+    expect(screen.queryByText("Option 3")).not.toBeInTheDocument();
+  });
+
+  it("should correctly display a React element provided as a selectedLabel", () => {
+    render(<CustomSelect options={options} value="4" onChange={() => {}} />);
+
+    const span = screen.getByTestId("selected-label-test-id");
+    expect(
+      within(span).getByText("🇯🇵 Selected label of 4"),
+    ).toBeInTheDocument();
+  });
+
+  it("should update the displayed label when the selection changes", () => {
+    const { rerender } = render(
+      <CustomSelect
+        options={options}
+        value="1" // Initially selected
+        onChange={() => {}}
+      />,
+    );
+
+    expect(screen.getByText("Option 1")).toBeInTheDocument();
+
+    rerender(
+      <CustomSelect
+        options={options}
+        value="3" // New selection
+        onChange={() => {}}
+      />,
+    );
+
+    expect(screen.getByText("Selected label of 3")).toBeInTheDocument();
+    expect(screen.queryByText("Option 1")).not.toBeInTheDocument();
   });
 });
