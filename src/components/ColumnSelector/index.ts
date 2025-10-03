46d0c82f8c9a31e52fd87f8b0d386c86547d15a3
Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>
2025-10-03 09:38:20 +0200

    feat(ColumnSelector): create a component that can toggle columns of a table
    Signed-off-by: Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>

---
+++ src/components/ColumnSelector/index.ts
@@ -0,0 +1,6 @@
+export { default } from "./ColumnSelector";
+export {
+  visibleHeaderColumns,
+  visibleRowColumns,
+} from "./columnSelectorHelper";
+export type { Props as ColumnSelectorProps } from "./ColumnSelector";
